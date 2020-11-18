ddd28e1269c0aab76b8b38d54db9b5caa7fd4782
Uli Raich <uli.raich@gmail.com>
2020-11-18 20:27:44 +0100

    updated second msgbox example

--- widgets/msgbox/ex2_mbox.py
+++ widgets/msgbox/ex2_mbox.py
@@ -1,7 +1,7 @@
 #!/opt/bin/lv_micropython
 import lvgl as lv
-import time
 import init_gui
+import time
 from lv_colors import *
 
 welcome_info = "Welcome to the modal message box demo!\nPress the button to display a message box."
@@ -10,8 +10,20 @@ in_msg_info = "Notice that you cannot touch the button again while the message b
 LV_HOR_RES=240
 LV_VER_RES=240
 
-def opa_anim(bg,v):
-    lv.obj.set_style_local_bg_opa(bg, lv.obj_.PART.MAIN, lv.STATE.DEFAULT, v)
+def opa_anim(mbox,v):
+    bg = lv.obj.__cast__(mbox)
+    # print("v: ",v)
+    mbox.get_parent().set_style_local_bg_opa(lv.obj.PART.MAIN, lv.STATE.DEFAULT, v)
+    
+def mbox_event_cb(obj, evt):
+    if evt == lv.EVENT.DELETE:
+        # Delete the parent modal background 
+        lv.obj.del_async(obj.get_parent())
+        info.set_text(welcome_info)
+        info.align(None, lv.ALIGN.IN_BOTTOM_LEFT, 5, -15)
+    elif evt == lv.EVENT.VALUE_CHANGED:
+        # a button was clicked 
+        obj.start_auto_close(0)
     
 def btn_event_cb(btn,evt):
     if evt == lv.EVENT.CLICKED:
@@ -22,8 +34,8 @@ def btn_event_cb(btn,evt):
         obj.reset_style_list(lv.obj.PART.MAIN)
         obj.add_style(lv.obj.PART.MAIN, style_modal)
         obj.set_pos(0, 0)
-        obj.set_size(LV_HOR_RES, LV_VER_RES);
-
+        obj.set_size(LV_HOR_RES, LV_VER_RES)
+        #obj.set_style_local_bg_opa(lv.obj.PART.MAIN, lv.STATE.DEFAULT, lv.OPA._80)
         btns2 = ["Ok", "Cancel", ""]
 
         # Create the message box as a child of the modal background 
@@ -31,20 +43,20 @@ def btn_event_cb(btn,evt):
         mbox.add_btns(btns2);
         mbox.set_text("Hello world!")
         mbox.align(None, lv.ALIGN.CENTER, 0, 0)
-        # mbox.set_event_cb(mbox_event_cb)
+        mbox.set_event_cb(mbox_event_cb)
 
         # Fade the message box in with an animation 
         a=lv.anim_t()
         a.init()
         a.set_var(obj)
         a.set_time(500)
-        a.set_values(lv.OPA.TRANSP, lv.OPA._50)
-        a.set_exec_cb(lambda a: opa_anim)
-        a.start()
+        a.set_values(lv.OPA.TRANSP, lv.OPA._70)
+        a.set_custom_exec_cb(lambda a, val: opa_anim(mbox,val))
+        lv.anim_t.start(a)
 
         info.set_text(in_msg_info)
         info.align(None, lv.ALIGN.IN_BOTTOM_LEFT, 5, -5)
-
+        
 style_modal = lv.style_t()
 style_modal.init()
 style_modal.set_bg_color(lv.STATE.DEFAULT, LV_COLOR_BLACK)
@@ -64,8 +76,8 @@ info = lv.label(lv.scr_act(), None)
 info.set_text(welcome_info)
 info.set_long_mode(lv.label.LONG.BREAK)       # Make sure text will wrap 
 info.set_width(LV_HOR_RES - 10)
-info.align(None, lv.ALIGN.IN_BOTTOM_LEFT, 5, -5)
-    
+info.align(None, lv.ALIGN.IN_BOTTOM_LEFT, 5, -15)
+
 while True:
     lv.task_handler()
     time.sleep_ms(5)
