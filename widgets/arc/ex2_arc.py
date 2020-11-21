ab6f9902a89a02fea887c68ab52347f306e86165
Uli Raich <uli.raich@gmail.com>
2020-11-21 11:01:45 +0100

    corrected ex2_arc.py

--- widgets/arc/ex2_arc.py
+++ widgets/arc/ex2_arc.py
@@ -3,8 +3,7 @@ import lvgl as lv
 import init_gui
 import time
 
-def arc_loader(task):
-    arc=lv.arc.__cast__(task.user_data)
+def arc_loader(task,arc):
     angle = arc.get_value()
     print("angle: ",angle)
     angle += 5
@@ -16,14 +15,19 @@ def arc_loader(task):
 # create an arc which acts as a loader
 
 arc = lv.arc(lv.scr_act(),None)
+arc.set_range(0,360)
 arc.set_bg_angles(0,360)
+arc.set_angles(0,360)
 arc.set_rotation(270)
 arc.align(None,lv.ALIGN.CENTER,0,0)
+arc.set_value(10)
 
 # Create an `lv_task` to update the arc.
-# Store the `arc` in the user data
-arc.set_value(10)
-lv.task_create(arc_loader, 20, lv.TASK_PRIO.LOWEST, arc)
+
+task = lv.task_create_basic()
+task.set_cb(lambda task: arc_loader(task, arc))
+task.set_period(20)
+task.set_prio(lv.TASK_PRIO.LOWEST)
 
 while True:
     lv.task_handler()
