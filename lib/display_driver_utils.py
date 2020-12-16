9c52d66865ac98886581d4818ee96b4d55499273
Uli Raich <uli.raich@gmail.com>
2020-12-16 10:42:46 +0100

    improved according to Amir's comments

--- lib/display_driver_utils.py
+++ lib/display_driver_utils.py
@@ -1,9 +1,12 @@
 import lvgl as lv
 lv.init()
 
+ORIENT_LANDSCAPE = False
+ORIENT_PORTRAIT  = True
+
 class driver:
     
-    def __init__(self,width=240,height=240,orientation="LANDSCAPE"):
+    def __init__(self,width=240,height=240,orientation=ORIENT_LANDSCAPE):
         self.width = width
         self.height = height
         self.orientation = orientation
@@ -39,32 +42,32 @@ class driver:
         self.type = "SDL"
         print("Running the SDL lvgl version")
         
-    def init_gui_esp32(self):
+    def init_gui_ili9341(self):
 
         import lvesp32
 
         # Initialize ILI9341 display
 
-        from ili9XXX import ili9341,COLOR_MODE_BGR,LANDSCAPE,PORTRAIT
+        from ili9XXX import ili9341,LANDSCAPE
         from xpt2046 import xpt2046
         import espidf as esp
-        if self.orientation == "PORTRAIT":
+        if self.orientation:
             print ("Running the ili9341 lvgl version in portait mode")
             # Initialize ILI9341 display in prtrait mode
-            self.disp = ili9341(miso=19,mosi=23,clk=18, cs=26, dc=5, rst=-1, power=-1, backlight=-1, backlight_on=0, power_on=0,
-                                spihost=esp.VSPI_HOST, mhz=40, factor=4, hybrid=True, width=240, height=320,
-                                colormode=COLOR_MODE_BGR, rot=PORTRAIT, invert=False, double_buffer=True, half_duplex=True)
+            self.disp = ili9341(miso=19,mosi=23,clk=18, cs=26, dc=5, rst=-1, power=-1, backlight=-1, spihost=esp.VSPI_HOST)
             self.touch = xpt2046(spihost=esp.VSPI_HOST,cal_x0=3751, cal_x1 = 210, cal_y0=3906, cal_y1 = 283, transpose=True)
+            self.type="ili9341"
+            # self.disp = ili9341()
             # self.touch = xpt2046()
         else:
             print ("Running the ili9341 lvgl version in landscape mode")
             # Initialize ILI9341 display
-            self.disp = ili9341(miso=19,mosi=23,clk=18, cs=26, dc=5, rst=-1, power=-1, backlight=-1, backlight_on=0, power_on=0,
-                                spihost=esp.VSPI_HOST, mhz=40, factor=4, hybrid=True, width=320, height=240,
-                                colormode=COLOR_MODE_BGR, rot=LANDSCAPE, invert=False, double_buffer=True, half_duplex=True)
+            self.disp = ili9341(miso=19,mosi=23,clk=18, cs=26, dc=5, rst=-1, power=-1, backlight=-1, backlight_on=0,
+                                spihost=esp.VSPI_HOST, width=320, height=240, rot=LANDSCAPE)
             
             # Register xpt2046 touch driver
             self.touch = xpt2046(spihost=esp.VSPI_HOST,cal_x0=3799, cal_x1 = 353, cal_y0=220, cal_y1 = 3719, transpose=False)
+            # self.disp(rot=LANDSCAPE)
             # self.touch = xpt2046(transpose = False)
             self.type="ili9341"
         # self.disp = ili9341()
@@ -83,23 +86,8 @@ class driver:
 
         # from xpt2046 import xpt2046
         # self.touch = xpt2046()
-    
-    def init_gui(self):
-        
-        # Identify platform and initialize it
 
-        try:
-            self.init_gui_esp32()
-        except ImportError:
-            pass
-    
-        try:
-            self.init_gui_SDL()
-        except ImportError:
-            pass
-
-        try:
-            # check if we are running on a t-watch
+    def init_gui_twatch(self):
             import ttgo
             from axp_constants import AXP202_VBUS_VOL_ADC1,AXP202_VBUS_CUR_ADC1,AXP202_BATT_CUR_ADC1,AXP202_BATT_VOL_ADC1
                 
@@ -114,6 +102,28 @@ class driver:
             watch.tft.backlight_fade(100)
             self.type="t-watch"            
             print("Running lvgl on the LilyGo t-watch 2020")
+            
+    def init_gui(self):
+        
+        # Identify platform and initialize it
+
+        try:
+            self.init_gui_twatch()
         except:
             pass
+
+        if not self.type:
+            try:
+                self.init_gui_ili9341()
+            except ImportError:
+                pass
+            
+        try:
+            self.init_gui_SDL()
+        except ImportError:
+            pass
+
+        if not self.type:
+            print("Could not find a suitable display driver")
+            
 
